ba98376eeabdec085da772a6e98bb223bcd25f90
Shadip Kumar Joshi <220049@softwarica.edu.np>
2025-06-12 15:12:46 +0545

    js in static

---
+++ core/static/core/js/script.js
@@ -0,0 +1,19 @@
+// static/core/js/script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  });
+});
